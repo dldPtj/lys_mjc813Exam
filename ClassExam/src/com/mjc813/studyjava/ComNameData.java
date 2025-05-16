2b153566f77a1ddc72a7b3c557eb924be74ff1bb
dldPtj <eyeseo723@naver.com>
2025-05-16 21:47:49 +0900

    ClassExam computer 1

---
+++ ClassExam/src/com/mjc813/studyjava/ComNameData.java
@@ -0,0 +1,9 @@
+package com.mjc813.studyjava;
+
+public enum ComNameData {
+    com1,
+    com2,
+    com3,
+    com4,
+    com5
+}
---
+++ ClassExam/src/com/mjc813/studyjava/Computer.java
@@ -0,0 +1,18 @@
+package com.mjc813.studyjava;
+
+import java.awt.color.CMMException;
+
+public class Computer {
+    public static void main(String[] args) {
+        String[] computer_cpu = {"Gen9_i5", "Gen9_i7", "Gen9_i9"};
+        String[] computer_ram = {"4GB", "8GB", "16GB", "32GB"};
+        String[] computer_storage = {"SSD", "HDD"};
+        String[] computer_storage_size = {"500GB", "1000GB", "2000GB"};
+        String[] computer_graphic = {"GTX_1660", "RTX_4080", "Intel_A770"};
+
+
+
+        System.out.printf("가 켜졌습니다.");
+    }
+
+}
