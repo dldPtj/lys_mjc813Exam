4df644feb077aca0c2b52e4f40d939a76b57b746
dldPtj <eyeseo723@naver.com>
2025-06-02 21:34:32 +0900

    11장 예외처리 확인문제

---
+++ chapter/src/LoginExample.java
@@ -0,0 +1,26 @@
+public class LoginExample {
+    public static void main(String[] args) {
+        try {
+            login("white", "120345");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            login("blu0e", "54321");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void login(String id, String password) throws Exception {
+        // id가 blue가 아니라면 NotExistIDException을 발생시킴
+        if (!id.equals("blue")) {
+            throw new NotExistIDException("아이디가 존재하지 않습니다.");
+        }
+        // password가 12345가 아니라면 WrongPasswordException을 발생시킴
+        if (!password.equals("12345")) {
+            throw new WrongPasswordException("패스워드가 틀립니다.");
+        }
+    }
+}
