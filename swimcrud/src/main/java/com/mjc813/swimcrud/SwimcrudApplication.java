ba3c42ca92d4a35a6d17b35f4ff02358f2702517
dldPtj <eyeseo723@naver.com>
2025-07-25 15:01:54 +0900

    swimcrud db 생성, yml 설정 완료

---
+++ swimcrud/src/main/java/com/mjc813/swimcrud/SwimcrudApplication.java
@@ -0,0 +1,13 @@
+package com.mjc813.swimcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwimcrudApplication.class, args);
+    }
+
+}
---
+++ swimcrud/src/test/java/com/mjc813/swimcrud/SwimcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjc813.swimcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
