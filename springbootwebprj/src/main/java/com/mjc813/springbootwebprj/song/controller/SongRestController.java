a1c8013b8f189fd17d66173c0b498d4738422b6f
dldPtj <eyeseo723@naver.com>
2025-08-12 16:42:59 +0900

    springbootwebprj 프로젝트 delete, findById, findByTitleContains 기능 jpa로 추가

--- springbootwebprj/src/main/java/com/mjc813/springbootwebprj/song/controller/SongRestController.java
+++ springbootwebprj/src/main/java/com/mjc813/springbootwebprj/song/controller/SongRestController.java
@@ -1,13 +1,18 @@
 package com.mjc813.springbootwebprj.song.controller;
 
 import com.mjc813.springbootwebprj.common.ResponseDto;
+import com.mjc813.springbootwebprj.genre.dto.GenreEntity;
 import com.mjc813.springbootwebprj.song.dto.SongEntity;
 import com.mjc813.springbootwebprj.song.service.SongRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -21,7 +26,7 @@ public class SongRestController {
         try {
             this.songRepository.save(entity);
             return ResponseEntity.ok().body(
-                    ResponseDto.builder().resultCode(999).message("ERROR")
+                    ResponseDto.builder().resultCode(999).message("SUCCESS")
                             .resultData(entity).build()
             );
         } catch(Throwable e) {
@@ -38,7 +43,7 @@ public class SongRestController {
         try {
             this.songRepository.save(entity);
             return ResponseEntity.ok().body(
-                    ResponseDto.builder().resultCode(999).message("ERROR")
+                    ResponseDto.builder().resultCode(999).message("SUCCESS")
                             .resultData(entity).build()
             );
         } catch(Throwable e) {
@@ -49,4 +54,75 @@ public class SongRestController {
             );
         }
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseDto> delete(@PathVariable Long id) {
+        try {
+            this.songRepository.deleteById(id);
+            return ResponseEntity.ok().body(
+                    ResponseDto.builder().resultCode(999).message("SUCCESS")
+                            .resultData(id).build()
+            );
+        } catch(Throwable e) {
+            log.error(e.toString());
+            return ResponseEntity.status(500).body(
+                    ResponseDto.builder().resultCode(999).message("ERROR")
+                            .resultData(id).build()
+            );
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseDto> findById(@PathVariable Long id) {
+        try {
+            Optional<SongEntity> find = this.songRepository.findById(id);
+            if (find.isPresent()) {
+                return ResponseEntity.ok().body(
+                        ResponseDto.builder().resultCode(999).message("SUCCESS")
+                        .resultData(id).build());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch(Throwable e) {
+            log.error(e.toString());
+            return ResponseEntity.status(500).body(
+                    ResponseDto.builder().resultCode(999).message("ERROR")
+                            .resultData(id).build()
+            );
+        }
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ResponseDto> findByTitleContains(@RequestParam String title, Pageable pageable) {
+        try {
+            Page<SongEntity> list = this.songRepository.findByTitleContains(title, pageable);
+            return ResponseEntity.ok().body(
+                    ResponseDto.builder().resultCode(999).message("SUCCESS")
+                            .resultData(list).build()
+            );
+        } catch (Throwable e) {
+            log.error(e.toString());
+            return ResponseEntity.status(500).body(
+                    ResponseDto.builder().resultCode(999).message("ERROR")
+                            .resultData(null).build()
+            );
+        }
+    }
+
+//    @GetMapping("")
+//    public ResponseEntity<ResponseDto> findByArtistContains(@RequestParam String name, Pageable pageable) {
+//        try {
+//            Page<SongEntity> list = this.songRepository.findByArtistContains(name, pageable);
+//            return ResponseEntity.ok().body(
+//                    ResponseDto.builder().resultCode(999).message("SUCCESS")
+//                            .resultData(list).build()
+//            );
+//        } catch (Throwable e) {
+//            log.error(e.toString());
+//            return ResponseEntity.status(500).body(
+//                    ResponseDto.builder().resultCode(999).message("ERROR")
+//                            .resultData(null).build()
+//            );
+//        }
+//    }
 }
--- springbootwebprj/src/main/java/com/mjc813/springbootwebprj/song/service/SongRepository.java
+++ springbootwebprj/src/main/java/com/mjc813/springbootwebprj/song/service/SongRepository.java
@@ -3,11 +3,14 @@ package com.mjc813.springbootwebprj.song.service;
 import com.mjc813.springbootwebprj.common.ResponseDto;
 import com.mjc813.springbootwebprj.genre.dto.GenreEntity;
 import com.mjc813.springbootwebprj.song.dto.SongEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SongRepository extends JpaRepository<SongEntity, Long> {
-
+    Page<SongEntity> findByTitleContains(String title, Pageable pageable);
+    Page<SongEntity> findByArtistContains(String name, Pageable pageable);
 }
