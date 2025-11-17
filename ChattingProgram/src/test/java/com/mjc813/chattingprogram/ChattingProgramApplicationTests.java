b715bca2ffc14c49797b5636571e2bd0bb09f435
dldPtj <eyeseo723@naver.com>
2025-11-17 17:29:23 +0900

    ChattingProgram
    채팅 프로그램 세팅 완료

---
+++ ChattingProgram/src/test/java/com/mjc813/chattingprogram/ChattingProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjc813.chattingprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
